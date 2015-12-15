ea13d2d433b365854cbe1ad59f0b0ef11626134b
Victor Jiao <VjiaoBlack@gmail.com>
2015-12-14 19:05:03 -0500

    Day3 with matrix works.

--- day3/first.c
+++ day3/first.c
@@ -3,156 +3,16 @@
 
 
 
-typedef struct col_t {
-    int pos;
-    struct col_t *next;
-    struct col_t *prev;
-} col_t;
-
-typedef struct row_t {
-    int pos;
-    struct row_t *next;
-    struct row_t *prev;
-    col_t* elements;
-} row_t;
-
-col_t* new_col() {
-    col_t* col = malloc(sizeof(col_t));
-
-    col->pos = 0;
-    col->next = NULL;
-    col->prev = NULL;
-
-    return col;
-}
-
-row_t* new_row() {
-    row_t* row = malloc(sizeof(row_t));
-
-    row->pos = 0;
-    row->next = NULL;
-    row->prev = NULL;
-    row->elements = new_col();
-    row->elements->pos = 0;
-
-    return row;
-}
-
-void add_house(row_t* base, int r, int c) { // inserts double linked list nodes
-    row_t* nr = base;
-    row_t* holdr;
-    if (r < nr->pos) {
-        while (r < nr->pos && nr->prev) {
-            nr = nr->prev;
-        }
-        if (nr->pos != r) {
-            holdr = nr->prev;
-            nr->prev = new_row();
-            nr->prev->pos = r;
-            nr->prev->elements->pos = c;
-            if (holdr) {
-                holdr->next = nr->prev;
-                nr->prev->prev = holdr;
-            }
-            nr = nr->prev;
-            return;
-        }
-    } else if (r > nr->pos) {
-        while (r > nr->pos && nr->next) {
-            nr = nr->next;
-        }
-        if (nr->pos != r) {
-            holdr = nr->next;
-            nr->next = new_row();
-            nr->next->pos = r;
-            nr->next->elements->pos = c;
-            if (holdr) {
-                holdr->prev = nr->next;
-                nr->next->next = holdr;
-            }
-            nr = nr->next;
-            return;
-        }
-    }
-
-    col_t* nc = nr->elements;
-    col_t* holdc;
-    if (c < nc->pos) {
-        while (c < nc->pos && nc->prev) {
-            nc = nc->prev;
-        }
-        if (nc->pos != c) {
-            holdc = nc->prev;
-            nc->prev = new_col();
-            nc->prev->pos = c;
-            if (holdc) {
-                holdc->next = nc->prev;
-                nc->prev->prev = holdc;
-            }
-            nc = nc->prev;
-        }
-    } else if (c > nc->pos) {
-        while (c > nc->pos && nc->next) {
-            nc = nc->next;
-        }
-        if (nc->pos != c) {
-            holdc = nc->next;
-            nc->next = new_col();
-            nc->next->pos = c;
-            if (holdc) {
-                holdc->prev = nc->next;
-                nc->next->next = holdc;
-            }
-            nc = nc->next;
-        }
-    }
-}
-
-int houses_in_row(row_t* row) {
-    int num = 1;
-    col_t* len = row->elements;     printf("  cur: %d, %d\n", row->pos, len->pos);
-    while ((len = len->prev)) {
-        ++num;                      printf("  pre: %d, %d\n", row->pos, len->pos);
-    }
-
-    len = row->elements;
-    while ((len = len->next)) {
-        ++num;                      printf("  pst: %d, %d\n", row->pos, len->pos);
-    }
-    return num;
-}
-
-int houses_in_base(row_t* base) {
-                                    printf("base:\n");
-    int num = houses_in_row(base);
-    row_t* len = base;
-    while ((len = len->prev)) {
-                                    printf("pre: %d\n", len->pos);
-        num += houses_in_row(len);
-    }
-
-    len = base;
-    while ((len = len->next)) {
-                                    printf("post: %d\n", len->pos);
-        num += houses_in_row(len);
-    }
-    return num;
-}
-
-
-
-
-
 int main() {
     char dir;
-    int total = 0;
     int r = 0, c = 0;
-    row_t* base = new_row(); // at 0, 0
+
+    int max_r = 0, max_c = 0, min_r = 0, min_c = 0;
+
+    char buf[10000] = {0};
+    int i = 0;
 
     printf("cat <input> | ./first\n");
-
-    add_house(base, 0, 0);
-                                    printf("0: %d, %d\n", r, c);
 
     while ((dir = getchar()) > 0) {
 
@@ -173,12 +33,59 @@ int main() {
                 continue; // ignore rest of while loop
         }
 
-        printf("%c: %d, %d\n", dir, r, c);
+        buf[i] = dir;
+        i++;
 
-        add_house(base, r, c);
+        if (c < min_c)
+            min_c = c;
+        else if (c > max_c)
+            max_c = c;
+
+        if (r < min_r)
+            min_r = r;
+        else if (r > max_r)
+            max_r = r;
+    }
+
+    int j;
+    c = 0 - min_c;
+    r = 0 - min_r;
+
+    char** array = malloc(sizeof(char*) * (max_r - min_r + 1));
+    int b;
+    for (b = 0; b < (max_r - min_r + 1); b++) {
+        array[b] = calloc(max_c - min_c + 1, sizeof(char));
+    }
+
+    array[r][c] = 1;
+    int total = 1;
+    for (j = 0; j < i; j++) {
+        switch (buf[j]) {
+            case '>':
+                ++c;
+                break;
+            case '^':
+                --r;
+                break;
+            case '<':
+                --c;
+                break;
+            case 'v':
+                ++r;
+                break;
+            default:
+                continue; // ignore rest of while loop
+        }
+
+        if (!array[r][c]) {
+            total++;
+            array[r][c] = 1;
+        }
+
+
     }
 
     // count total, and print.
     // Keeping a tally as I add houses would be more efficent, however.
-    printf("total: %d\n", houses_in_base(base));
+    printf("total: %d\n", total);
 }
--- day3/second.c
+++ day3/second.c
@@ -1,23 +1,114 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+
 int main() {
-    int x, y, z, minp, vol;
-    int total = 0;
+    char dir;
+    int r[2] = {0};
+    int c[2] = {0};
 
-    printf("cat <input> | ./first\n");
-    while (scanf("%dx%dx%d", &x, &y, &z) > 0) {
-        vol = x * y * z;
+    int max_r[2] = {0};
+    int max_c[2] = {0};
+    int min_r[2] = {0};
+    int min_c[2] = {0};
 
-        if (z >= x && z >= y) {
-            minp = 2 * x + 2 * y;
-        } else if (x >= z && x >= y) {
-            minp = 2 * z + 2 * y;
-        } else {
-            minp = 2 * z + 2 * x;
+    int is_santa = 1;
+
+    char buf[10000] = {0};
+    int i = 0;
+
+    printf("cat <input> | ./second\n");
+
+    while ((dir = getchar()) > 0) {
+
+        switch (dir) {
+            case '>':
+                ++c[is_santa];
+                break;
+            case '^':
+                --r[is_santa];
+                break;
+            case '<':
+                --c[is_santa];
+                break;
+            case 'v':
+                ++r[is_santa];
+                break;
+            default:
+                continue; // ignore rest of while loop
         }
 
-        total += minp + vol;
+        buf[i] = dir;
+        i++;
+
+        if (c[is_santa] < min_c[is_santa])
+            min_c[is_santa] = c[is_santa];
+        else if (c[is_santa] > max_c[is_santa])
+            max_c[is_santa] = c[is_santa];
+
+        if (r[is_santa] < min_r[is_santa])
+            min_r[is_santa] = r[is_santa];
+        else if (r[is_santa] > max_r[is_santa])
+            max_r[is_santa] = r[is_santa];
+
+        is_santa = !is_santa;
     }
-    printf("%d\n", total);
+
+    int j;
+
+
+    int max_rt = max_r[1] > max_r[0] ? max_r[1] : max_r[0];
+    int max_ct = max_c[1] > max_c[0] ? max_c[1] : max_c[0];
+    int min_rt = min_r[1] < min_r[0] ? min_r[1] : min_r[0];
+    int min_ct = min_c[1] < min_c[0] ? min_c[1] : min_c[0];
+
+
+    c[is_santa] = 0 - min_ct;
+    r[is_santa] = 0 - min_rt;
+    c[!is_santa] = 0 - min_ct;
+    r[!is_santa] = 0 - min_rt;
+
+
+    int num_r = max_rt - min_rt + 1;
+    int num_c = max_ct - min_ct + 1;
+
+    char** array = malloc(sizeof(char*) * num_r);
+    int b;
+    for (b = 0; b < num_r; b++) {
+        array[b] = calloc(num_c, sizeof(char));
+    }
+    is_santa = 1;
+    array[r[is_santa]][c[is_santa]] = 1;
+    int total = 1;
+    for (j = 0; j < i; j++) {
+        switch (buf[j]) {
+            case '>':
+                ++c[is_santa];
+                break;
+            case '^':
+                --r[is_santa];
+                break;
+            case '<':
+                --c[is_santa];
+                break;
+            case 'v':
+                ++r[is_santa];
+                break;
+            default:
+                continue; // ignore rest of while loop
+        }
+
+        if (!array[r[is_santa]][c[is_santa]]) {
+            total++;
+            array[r[is_santa]][c[is_santa]] = 1;
+        }
+
+
+        is_santa = !is_santa;
+    }
+
+    // count total, and print.
+    // Keeping a tally as I add houses would be more efficent, however.
+    printf("total: %d\n", total);
 }
